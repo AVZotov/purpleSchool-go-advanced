f3c371a15374cef02b2facf203973ae2ad38db4d
Alexey Zotov <alexey1.zotov@yandex.ru>
2025-06-29 11:44:02 +0300

    bug fix with rand generation

--- http_server/utils/random.go
+++ http_server/utils/random.go
@@ -1,9 +1,14 @@
 package utils
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 func RandomInt() int {
+	var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 	minValue := 1
 	maxValue := 7
-	return minValue + rand.Intn(maxValue-minValue)
+	return minValue + rnd.Intn(maxValue-minValue)
 }
