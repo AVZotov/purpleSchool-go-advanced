bcf6fea85e17881741f4353510700f5232575c5d
Alexey Zotov <alexey1.zotov@yandex.ru>
2025-07-24 19:34:15 +0300

    BigFix: DTO tags changed

--- order_api_auth/internal/domain/auth/session/dto.go
+++ order_api_auth/internal/domain/auth/session/dto.go
@@ -6,7 +6,7 @@ type SendCodeRequest struct {
 
 type VerifyCodeRequest struct {
 	SessionID string `json:"sessionId" validate:"required,hexadecimal,len=64"`
-	Code      int    `json:"code" validate:"required,min=1000,max=9999"`
+	Code      int    `json:"code" validate:"required,gte=1000,lte=9999"`
 }
 
 type ResponseWithSession struct {
--- order_api_auth/internal/domain/auth/session/model.go
+++ order_api_auth/internal/domain/auth/session/model.go
@@ -8,5 +8,5 @@ type Session struct {
 	gorm.Model
 	Phone     string `json:"phone,omitempty" validate:"omitempty,e164" gorm:"index"`
 	SessionID string `json:"sessionId,omitempty" validate:"omitempty,hexadecimal,len=64" gorm:"uniqueIndex;size:64"`
-	SMSCode   int    `json:"code,omitempty" validate:"omitempty,min=1000,max=9999" gorm:""`
+	SMSCode   int    `json:"code,omitempty" validate:"omitempty,gte=1000,lte=9999" gorm:""`
 }
